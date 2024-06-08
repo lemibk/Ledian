62c7210e03a05cdebf20370e8204daff490b0578
lemibk <lemibidikasenbeta@gmail.com>
2024-06-08 19:29:31 +0300

    starting backend

---
+++ backend/signup.php
@@ -0,0 +1,29 @@
+<?php
+// Include config file
+require "Configfile.php";
+
+// Get form data
+$user_name = $_POST['username'];
+$first_name = $_POST['firstname'];
+$last_name = $_POST['lastname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Hash the password
+$hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+// Prepare and bind
+$stmt = $link->prepare("INSERT INTO logins (username, firstname, lastname, email, password) VALUES (?, ?, ?, ?, ?)");
+$stmt->bind_param("sssss", $user_name, $first_name, $last_name, $email, $hashed_password);
+
+// Execute the statement
+if ($stmt->execute()) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $stmt->error;
+}
+
+// Close the connection
+$stmt->close();
+$link->close();
+?>
